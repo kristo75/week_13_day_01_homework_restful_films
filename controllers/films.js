b81d3374ba1b4865efb174c557bc65cca5db0fba
Kris <kristopher_anderson@icloud.com>
2018-02-27 09:38:12 +0000

    added comments and review from hw solution

--- controllers/films.js
+++ controllers/films.js
@@ -7,15 +7,18 @@ const Film = require('../client/src/models/film');
 const Review = require('../client/src/models/review');
 
 
-
+//index
 filmRouter.get('/', function(req, res){
   res.json({data: films});
-})
+});
 
+// show
 filmRouter.get('/:id', function (req, res){
   res.json({data: films[req.params.id]});
-})
+});
 
+
+//create
 filmRouter.post('/', function(req, res) {
   var film = new Film({
     title: req.body.title,
@@ -25,6 +28,7 @@ filmRouter.post('/', function(req, res) {
   res.json({data: films});
 });
 
+//update
 filmRouter.put('/:id', function(req, res){
   const film = new Film({
     title: req.body.title,
@@ -33,12 +37,25 @@ filmRouter.put('/:id', function(req, res){
   films[req.params.id] = film;
   res.json({data: films});
 
-})
+});
 
+// delete
 filmRouter.delete('/:id', function(req, res){
   films.splice(req.params.id, 1);
   res.json({data: films});
-})
+});
+
+//create review
+filmRouter.post('/:id/reviews', function(req, res) {
+  const film = films[req.params.id];
+  const review1 = new Review({
+    comment: req.body.comment,
+    rating: req.body.rating,
+    author: req.body.author
+  });
+  film.addReview(review1);
+  res.json({data: films});
+});
 
 
 module.exports = filmRouter;
